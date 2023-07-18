ed3673bf99545a4fdfab97654ef7d529dda03ecd
nelsonkimaiga <nelsonkimaiga@aol.com>
2023-07-18 20:29:12 +0300

    bash script refine

--- national/national_script.sh
+++ national/national_script.sh
@@ -1,38 +1,27 @@
 #!/bin/bash
 
-# Start dhis_pss_national service
-docker run -d --name dhis_pss_national \
-  -e "PATH=/usr/local/openjdk-11/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
-  -e "JAVA_HOME=/usr/local/openjdk-11" \
-  -e "LANG=C.UTF-8" \
-  -e "JAVA_VERSION=11.0.16" \
-  -p 7001:7001 \
-  intellisoftdev/dhis_pss_national:v1 \
-  java -jar target/PssNationalInstance.jar
+# build Docker images from Dockerfiles
+build_images() {
+  echo "Building Docker images..."
+  docker build -t dhis2/core:2.38.0 -f Dockerfile-core .
+  docker build -t ghcr.io/baosystems/postgis:12-3.3 -f Dockerfile-db .
+  docker build -t jwilder/nginx-proxy:alpine -f Dockerfile-gateway .
+  docker build -t intellisoftdev/dhis_pss_national:v1 -f Dockerfile-dhis_pss_national .
+  docker build -t intellisoftdev/pss_data_sync:latest -f Dockerfile-pss_data_synchronization .
+  docker build -t intellisoftdev/pss-survey -f Dockerfile-survey-app .
+}
 
-# Wait for dhis_pss_national service to start
-sleep 10
+# execute Docker Compose commands
+execute_compose() {
+  echo "Executing Docker Compose..."
+  docker-compose -p d2-cluster-2380 up -d
+}
 
-# Start pss_data_synchronization service
-docker run -d --name pss_data_synchronization \
-  -e "SOURCE_USERNAME=admin" \
-  -e "SOURCE_PASSWORD=district" \
-  -e "SOURCE_URL=https://pssnational.intellisoftkenya.com/api/events" \
-  -e "TARGET_USERNAME=admin" \
-  -e "TARGET_PASSWORD=district" \
-  -e "TARGET_URL=https://pssinternational.intellisoftkenya.com/api/events" \
-  -e "SOURCE_DATA_ELEMENTS_URL=https://pssnational.intellisoftkenya.com/api/dataElements" \
-  -e "PATH=/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
-  -e "LANG=C.UTF-8" \
-  -e "GPG_KEY=E3FF2839C048B25C084DEBE9B26995E310250568" \
-  -e "PYTHON_VERSION=3.9.17" \
-  -e "PYTHON_PIP_VERSION=23.0.1" \
-  -e "PYTHON_SETUPTOOLS_VERSION=58.1.0" \
-  -e "PYTHON_GET_PIP_URL=https://github.com/pypa/get-pip/raw/0d8570dc44796f4369b652222cf176b3db6ac70e/public/get-pip.py" \
-  -e "PYTHON_GET_PIP_SHA256=96461deced5c2a487ddc65207ec5a9cffeca0d34e7af7ea1afc470ff0d746207" \
-  -p 8000:8000 \
-  intellisoftdev/pss_data_sync:latest \
-  cron -f
+# Main function
+main() {
+  build_images
+  execute_compose
+}
 
-# Stop and remove containers
-docker rm -f dhis_pss_national pss_data_synchronization
+# Call the main function
+main
